cf7ae1993f948a8df788589a2fe0652709ef8177
Kei Vision <keivision@mail.ru>
2023-08-23 13:18:32 +0300

    Semantics update

--- engine/train.py
+++ engine/train.py
@@ -21,11 +21,12 @@ class TrainModel:
         self.y_days = self.df[['Hum_days']].values.ravel()
         self.y_volume = self.df[['Process_volume']].values.ravel()
 
-        self.X_train, self.X_test, self.y_train_days, self.y_test_days, self.y_train_volume, self.y_test_volume = train_test_split(self.X, 
-                                                                                                                                   self.y_days,
-                                                                                                                                   self.y_volume,
-                                                                                                                                   test_size = 0.2, 
-                                                                                                                                   random_state = 93)
+        (self.X_train,        self.X_test,
+         self.y_train_days,   self.y_test_days, 
+         self.y_train_volume, self.y_test_volume) = train_test_split(self.X, self.y_days, self.y_volume,
+                                                                        test_size = 0.2, random_state = 93)
+        
+
         self.model = Model()
         self.model.create_model()
         self.model.fit_model(self.X_train,
